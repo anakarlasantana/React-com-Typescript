9e6ce50103876f80517903666d3f1e222cb52c6b
Ana Karla <ana.karla@portfoliotech.com.br>
2023-02-22 17:08:35 -0300

    feat: useRef and components

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,29 +1,38 @@
-import { useState } from "react";
+import {  useCallback, useMemo, useRef, useState } from "react";
+import { InputLogin } from "./components/InputLogin";
 
-export const Login = () => {
+export const Login = () => { 
+    const inputPasswordRef = useRef<HTMLInputElement>(null);
 
     const [email, setEmail] = useState('');
     const [senha, setSenha] = useState('');
 
-    const handleClick = () => {
-        console.log('email:', email)
+    /*useEffect(()=> {
+        if (window.confirm('Você é homem?')) {
+            console.log('Homem')
+        } else {
+            console.log('Mulher')
+        }
+    }, [email, senha]);*/
 
-    }
+    const emailLength = useMemo(()=> {
+        return email.length*1000;
+    }, [email.length]);
+
+    const handleClick = useCallback(() => {
+        console.log(email)
+        console.log(senha);
+        
+    }, [email, senha])
 
 
     return (
         <div>
             <form action="submit">
-                <label>
-                    <span>Email</span>
-                    <input value={email} onChange={e => setEmail(e.target.value)}/>
-                </label>
-
-                <label>
-                    <span>Senha</span>
-                    <input type="password" value={senha} onChange={e => setSenha(e.target.value)}/>
-                </label>
-
+                <p>Quantidade de caracteres no email: {emailLength}</p>
+                <InputLogin label="Email" value={email} onChange={newValue => setEmail(newValue)} onPressEnter={() => inputPasswordRef.current?.focus()} />
+                <InputLogin label="Senha" value={senha} type="password" onChange={newValue => setSenha(newValue)}/>
+                
                 <button type="button" onClick={handleClick}>Entrar</button>
             </form>
         </div>
